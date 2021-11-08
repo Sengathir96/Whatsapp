f21adb1b739e989419231980d0689cd279836978
sengathir96 <sengathir100@gmail.com>
2021-11-08 19:45:12 +0530

    test

---
+++ src/org/git/Add.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class Add {
+	
+	
+	public static void add1(int a, int b) {
+		
+		int c = a+b;
+		
+		System.out.println(c);
+
+	}
+
+	public static void main(String[] args) {
+		
+		add1(10,20);
+
+	}
+
+}
